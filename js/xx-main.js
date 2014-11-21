ca580c3c45c58ef871915dd9a5a80aa1ab24f233
Kyle Brumm <kyle@webspecdesign.com>
2014-11-21 14:43:58 -0600

    More stuff

--- js/xx-main.js
+++ js/xx-main.js
@@ -8,11 +8,25 @@ $(function() {
 			maxFont   : 16,
 		});
 
-	$(document).on('click', '#main-navigation li a, .top, .navbar-brand', function() {
+	$(document).on('click', '#main-navigation li a, .top, .navbar-brand, .overview-links a', function() {
 		var section = $(this).attr('href'),
 				extra = -9;
-		$('html, body').animate({scrollTop:$(section).offset().top+extra},{duration:1000});
+		$('html, body').animate({scrollTop:$(section).offset().top+extra},{duration:500});
 		return false;
 	});
+
+	setTop();
+	$(window).scroll(function() {
+		setTop();
+	});
+
+	function setTop() {
+		var $top = $('.top');
+		if($(this).scrollTop() > 0) {
+			$top.addClass('visible');
+		} else {
+			$top.removeClass('visible');
+		}
+	}
 
 });
