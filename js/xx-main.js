47a192135c74947bc0e67ba581f60981fa9e569e
Kyle Brumm <kyle@webspecdesign.com>
2014-11-21 11:51:00 -0600

    More styling

--- js/xx-main.js
+++ js/xx-main.js
@@ -1,3 +1,18 @@
 $(function() {
-	$('body').scrollspy({ target: '.navbar-collapse' })
+	$('body')
+		.scrollspy({ target: '.navbar-collapse' })
+		.flowtype({
+			minimum   : 320,
+			maximum   : 780,
+			minFont   : 12,
+			maxFont   : 16,
+		});
+
+	$(document).on('click', '#main-navigation li a, .top, .navbar-brand', function() {
+		var section = $(this).attr('href'),
+				extra = -9;
+		$('html, body').animate({scrollTop:$(section).offset().top+extra},{duration:1000});
+		return false;
+	});
+
 });
